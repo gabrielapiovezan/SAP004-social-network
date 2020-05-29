cac2951cb270bdf4f4b97556396299ff79b1f1bc
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-05-29 15:40:40 -0300

    Login funcionando

---
+++ public/login.js
@@ -0,0 +1,26 @@
+function uiConfig() {
+    return {
+        signInFlow: 'popup',
+        signInSuccessUrl: '#',
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ]
+    }
+}
+
+function configLogin() {
+    ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebase-auth-container', uiConfig());
+}
+
+function removeLogin() {
+    document.getElementById('firebase-auth-container').innerHTML = `
+    Que bom ver vocdê ${firebase.auth().currentUser.displayName}
+    <a href="#" onClick="logout()";>Logout </a>`
+}
+
+function logout() {
+    firebase.auth().signOut();
+    location.reload()
+}
