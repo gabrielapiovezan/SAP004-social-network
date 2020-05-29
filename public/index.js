0108de1e8c55463d4c564cfc62dec52219606f2f
Camila Cunha <millagerarde@gmail.com>
2020-05-29 15:50:34 -0300

    Login

--- public/index.js
+++ public/index.js
@@ -1,9 +1,4 @@
 // // Este é o ponto de entrada de sua aplicação
 // import { home } from './pages/home/main.js';
 
-<<<<<<< HEAD
-document.querySelector('#root').appendChild(home());
-// TESTANDO
-=======
 // document.querySelector('#root').appendChild(home())
->>>>>>> 476f3b515ded06daa021cb9c988a5e75de058e2a
