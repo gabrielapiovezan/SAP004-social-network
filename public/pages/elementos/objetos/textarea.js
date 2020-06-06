9a7530006c4da7ea9236d3ec5425133bfd78d8c4
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-06-06 04:38:10 -0300

    Atualizações

--- public/pages/elementos/objetos/textarea.js
+++ public/pages/elementos/objetos/textarea.js
@@ -1,9 +1,5 @@
 export const textarea = (info) => {
     const textarea = `
-<<<<<<< HEAD
-    <textarea type="${info.type}" class="textarea ${info.class}" id="${info.id}" placeholder="${info.placeholder}">`;
-=======
-    <textarea type="${info.type}" class="textarea ${info.class}" rows="${info.rows}" cols="${info.cols}" id="${info.id}" placeholder="${info.placeholder}"></textarea>`;
->>>>>>> 3a8055ee7cc79e8daeda36c8666b45a4706666b7
+    <textarea type="${info.type}" size="${info.size}" class="textarea ${info.class}" id="${info.id}" placeholder="${info.placeholder}"></textarea>`;
     return textarea;
 }
