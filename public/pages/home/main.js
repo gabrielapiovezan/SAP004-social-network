1b8de7a2d30789675c7f7187bae2a132b05af539
Gabriela Piovezan Mendonça <61169682+gabrielapiovezan@users.noreply.github.com>
2020-06-06 04:14:24 -0300

    Atualizações no layout

--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,17 +1,20 @@
 import {greeting, logout, user} from './data.js';
 import {button} from '../elementos/objetos/button.js';
-// import { textarea } from '../elementos/objetos/textarea.js';
+import { input } from '../elementos/objetos/input.js';
 import {link} from '../elementos/objetos/link.js';
 import {textarea} from '../elementos/objetos/textarea.js';
+
 
 // ${textarea({ id: "name", type: "text", placeholder: "Diga Oi!" })} <textarea rows="52" cols="52" id="name" type="text"></textarea>
 
 export default () => {
   const container = document.createElement('div');
+  container.classList.add("container");
   container.innerHTML = `
     <form class="box">
-    ${textarea({ id: "name", type: "text", rows: "5", cols: "50", placeholder: "Diga Oi!" })}    
-    ${button({ id: "greeting-btn", class: "greeting-btn", name: "Dizer Oi" })}
+    ${textarea({ id: "name", type: "text", size:"500", placeholder: "Diga Oi!" })}
+    ${input({ type: "image", src: "./pages/elementos/icones/cereja-1.png", id: "gmailBtn", class: "icon" })}    
+    ${button({ id: "greeting-btn", class: "greeting-btn", name: "Compartilhar" })}
     </form>
     <div id='greeting-message'></div>
     <div id="firebase-auth-container"></div>
