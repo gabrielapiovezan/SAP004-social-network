d21a8b25cd2af5c30f2e6f03d58bafe44c62bbf8
Camila Cunha <millagerarde@gmail.com>
2020-06-05 22:56:00 -0300

    tentativa de mostrar usuario

--- public/pages/home/data.js
+++ public/pages/home/data.js
@@ -1,8 +1,20 @@
 export const greeting = name => `Oi ${name}! Que bom ver você aqui!`;
 
 export const logout = () => {
-    firebase.auth().signOut();
-    location.reload();
+  firebase.auth().signOut();
+  location.reload();
 };
+
+export const user = () => {
+  firebase.auth().onAuthStateChanged(function (user) {
+    var user = firebase.auth().currentUser;
+    if (user) {
+      let name = user.displayName;
+      console.log(`Oi ${name}! Que bom ver você aqui!`);
+    } else {
+      console.log("não possui usuário logado!");
+    }
+  });
+}
 
 //Que bom ver você ${firebase.auth().currentUser.displayName}
--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,15 +1,25 @@
 // Aqui serão criados os eventos de Manipulação de DOM e templates
-import { greeting, logout } from './data.js';
-import { button } from '../elementos/objetos/button.js';
+import {
+  greeting,
+  logout,
+  user
+} from './data.js';
+import {
+  button
+} from '../elementos/objetos/button.js';
 // import { textarea } from '../elementos/objetos/textarea.js';
-import { link } from '../elementos/objetos/link.js';
-import { textarea } from '../elementos/objetos/textarea.js';
+import {
+  link
+} from '../elementos/objetos/link.js';
+import {
+  textarea
+} from '../elementos/objetos/textarea.js';
 
 // ${textarea({ id: "name", type: "text", placeholder: "Diga Oi!" })} <textarea rows="52" cols="52" id="name" type="text"></textarea>
 
 export default () => {
-    const container = document.createElement('div');
-    container.innerHTML = `
+  const container = document.createElement('div');
+  container.innerHTML = `
     <form class="box">
     ${textarea({ id: "name", type: "text", rows: "5", cols: "50", placeholder: "Diga Oi!" })}    
     ${button({ id: "greeting-btn", class: "greeting-btn", name: "Dizer Oi" })}
@@ -18,21 +28,24 @@ export default () => {
     <div id="firebase-auth-container"></div>
     ${link({ href: "#", id: "buttonOut", name: "Logout" })}`;
 
-    const name = container.querySelector('#name');
-    const greetingBtn = container.querySelector('#greeting-btn');
-    const greetingMessage = container.querySelector('#greeting-message');
+  const name = container.querySelector('#name');
+  const greetingBtn = container.querySelector('#greeting-btn');
+  const greetingMessage = container.querySelector('#greeting-message');
 
-    greetingBtn.addEventListener('click', (event) => {
-        event.preventDefault();
+  greetingBtn.addEventListener('click', (event) => {
+    event.preventDefault();
 
-        greetingMessage.innerHTML = greeting(name.value);
-    });
+    greetingMessage.innerHTML = greeting(name.value);
+  });
 
-    container.querySelector("#buttonOut").addEventListener("click", (event) => {
-        event.preventDefault();
-        logout();
-        window.location.hash = "login";
-        location.reload();
-    });
-    return container;
+  container.querySelector("#buttonOut").addEventListener("click", (event) => {
+    event.preventDefault();
+    logout();
+    window.location.hash = "login";
+    location.reload();
+  });
+
+  user()
+
+  return container;
 };
