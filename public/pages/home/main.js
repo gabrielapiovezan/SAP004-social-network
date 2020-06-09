b2426cd68510323de02281a7b8dbac038a02f7f0
Camila Cunha <millagerarde@gmail.com>
2020-06-09 18:27:29 -0300

    .

--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,20 +1,27 @@
 import { greeting, logout, user } from './data.js';
-//import { button } from '../elementos/objetos/button.js';
-import button from '../elementos/objetos/button.js';
+import { button } from '../elementos/objetos/button.js';
 import { input } from '../elementos/objetos/input.js';
-import link from '../elementos/objetos/link.js';
+import { link } from '../elementos/objetos/link.js';
 import icon from '../elementos/objetos/icon.js';
-import textarea from '../elementos/objetos/textarea.js';
+import { textarea } from '../elementos/objetos/textarea.js';
 
-
+// ${textarea({ id: "name", type: "text", placeholder: "Diga Oi!" })} <textarea rows="52" cols="52" id="name" type="text"></textarea>
+//    ${icon('churrasqueira')}
 export default () => {
     const container = document.createElement('div');
     container.classList.add("container");
-
-    const greetingBtn = button({ id: "greeting-btn", class: "greeting-btn", name: "Compartilhar" })
-    const cherry = icon('cereja')
-    const textBox = textarea({ id: "name", type: "text", size: "500", placeholder: "Diga Oi!" })
-    const logout = link({ href: "#", id: "buttonOut", name: "Logout" })
+    container.innerHTML = `
+    <form class="box">
+    ${textarea({ id: "name", type: "text", size:"500", placeholder: "Diga Oi!" })}
+    <div class="space-buttons">
+    ${button({ id: "greeting-btn", class: "greeting-btn", name: "Compartilhar" })}
+    </div>
+    </form>
+    
+    <div id='greeting-message'></div>
+    <div id="firebase-auth-container"></div>
+    <div id='message'></div>
+    ${link({ href: "#", id: "buttonOut", name: "Logout" })}`;
 
     container.appendChild(icon('cereja'))
         // container.appendChild(icon('churrasqueira'))
@@ -25,49 +32,19 @@ export default () => {
         // container.appendChild(icon('tomate'))
         // container.appendChild(icon('caneca'))
 
-
-    const form = document.createElement('form')
-    form.classList.add("box")
-    form.innerHTML = textBox.element
-    form.appendChild(spaceButtons)
-    container.appendChild(form)
-
-    const greetingMessage = document.createElement('div')
-        // greetingMessage.classList.add("greeting-message");
-    container.appendChild(greetingMessage)
-
-    const back = document.createElement('div')
-    back.innerHTML += logout.element
-    container.appendChild(back)
+    const name = container.querySelector('#name');
+    const greetingBtn = container.querySelector('#greeting-btn');
+    const greetingMessage = container.querySelector('#greeting-message');
 
 
 
-    container.querySelector(cherry.id1).addEventListener("click", () => {
-        container.querySelector(cherry.id2).style.display = "block"
-        container.querySelector(cherry.id1).style.display = "none"
-    })
-    container.querySelector(cherry.id2).addEventListener("click", () => {
-        container.querySelector(cherry.id1).style.display = "block"
-        container.querySelector(cherry.id2).style.display = "none"
-    })
 
-
-    // container.appendChild(icon('cereja'))
-    // container.appendChild(icon('churrasqueira'))
-    // container.appendChild(icon('cafeteira'))
-    // container.appendChild(icon('comida'))
-    // container.appendChild(icon('luva'))
-    // container.appendChild(icon('talher'))
-    // container.appendChild(icon('tomate'))
-    // container.appendChild(icon('caneca'))
-
-
-    const textBoxElement = container.querySelector(textBox.id)
-
-    container.querySelector(greetingBtn.id).addEventListener("click", () => {
+    greetingBtn.addEventListener('click', (event) => {
         event.preventDefault();
-        greetingMessage.innerHTML = greeting(textBoxElement.value);
+        greetingMessage.innerHTML = greeting(name.value);
     });
+
+
 
     user();
 
