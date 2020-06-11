cff454bd7b66c080e540517e789cac378afe509e
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-06-11 06:03:51 -0300

    Erro do nome do usuário

--- public/pages/register/data.js
+++ public/pages/register/data.js
@@ -1,15 +1,21 @@
 export const register = (email, password, username, printErrorLogin) => {
-    firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
-        window.location.hash = "home";
-        firebase.auth().currentUser.updateProfile({
-            displayName: username
-        })
-    }).catch(function(error) {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        printErrorLogin(errorMessage)
-            // window.alert(errorMessage);
-            // ...
-    });
+    if (username) {
+        console.log("tem nome")
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(function(user) {
+                window.location.hash = "home";
+                firebase.auth().currentUser.updateProfile({
+                    displayName: username
+                })
+            }).catch(function(error) {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                printErrorLogin(errorMessage)
+                    // window.alert(errorMessage);            // ...
+            });
+    } else {
+        printErrorLogin("Name not typed")
+    }
 }
