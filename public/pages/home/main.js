a4850caefafc5f9c627eb30c561e0ed9e9d4e021
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-06-13 15:26:19 -0300

    Código Gabi

--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,126 +1,247 @@
-import { logout, loadPost, dataUser, updateCollection } from './data.js';
+import { user, createPost, logout, loadPost, dataUser, updateCollection, postDelete, updatePost } from './data.js';
 import { button } from '../elementos/objetos/button.js';
-//import { input } from '../elementos/objetos/input.js';
 import { link } from '../elementos/objetos/link.js';
+import iconColor from '../elementos/objetos/icon-dynamic.js';
 import icon from '../elementos/objetos/icon.js';
 import { textarea } from '../elementos/objetos/textarea.js';
+import { image } from '../elementos/objetos/image.js';
 
 
 export default () => {
-
     const container = document.createElement('div');
+    container.classList.add("container-home");
 
-    container.classList.add("container");
     container.innerHTML = `
-    <div id="nameUser"></div>
-    <form class="box">
-    ${textarea({ id: "post-text", type: "text", size:"500", placeholder: "Diga Oi!" })}
-    <div class="space-buttons">
-    ${button({ id: "greeting-btn", class: "greeting-btn", name: "Compartilhar" })}
-    </div>
-    </form>
-    <div id='greeting-message'></div>
-    <div id="firebase-auth-container"></div>
-    <div id='message'></div>
-    <ul id="posts" class="post-box"></ul>
-    ${link({ href: "#", id: "buttonOut", name: "Logout" })}`;
+    <header>
+      <nav role="navigation">
+        <div id="menuToggle">
+          <input type="checkbox"/>
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id="menu">
+          <li>${link({ href: "#profile", name: "Perfil", title: "perfil", target: "_self" })}</li>
+          <li>${link({ id: "logout-btn", name: "Sair", title: "deslogar", target: "_self" })}</li>
+          </ul>
+        </div>
+      </nav>
+      <h1> &lt; Umâmi &gt; </h1> 
+      <figure>
+        ${image({ src: "/pages/elementos/imagens/logo.png", class: "img-header", alt: "logo-umâmi" })}
+      </figure>
+    </header>
+    <section class="timeline">
+      <div class="profile">
+        <figure>
+          ${image({ src: "/pages/elementos/imagens/chefe.png", class: "img-profile", alt: "foto-perfil" })}
+        </figure>
+        <h3 id="nameUser" class="name-user"></h3>
+      </div>
+      ${image({src:"/pages/elementos/imagens/fundo.png", class:"disappear image-back"})}
+      <div class="posts">
+        <form class="box">
+          ${textarea({ id: "post-text", type: "text", size: "500", placeholder: "Compartilhe sua publicação aqui!" })}
+          <span>
+            <span id="loker">${iconColor({ name: 'cadeado', id:"block"})}</span>
+            ${icon({ name: 'img' })}
+            ${button({ id: "post-btn", class: "post-btn", name: "Postar" })}
+          </span>
+        </form>
+        <ul id="posts" class="post-box"></ul>
+      </div>
+    </section>
+    <footer class="footer">
+      <h5>Desenvolvido por:
+        <div>
+          ${link({ href: "", name: "Camila Cunha", class: "link-footer", title: "Camila Cunha", target: "_blank" })}
+            <span> 
+            ${link({ href: "https://github.com/camilagerarde", name: icon({ name: 'github' }), class: "link-footer", title: "Camila Cunha", target: "_blank" })}  
+            ${link({ href: "https://www.linkedin.com/in/camila-gerarde/", name: icon({ name: 'linkedin' }), class: "link-footer", title: "Camila Cunha", target: "_blank" })}
+            </span>
+          </div>
+        <div>
+          ${link({ href: "", name: "Gabriela Piovezan", class: "link-footer", title: "Gabriela Piovezan", target: "_blank" })}
+          <span>
+            ${link({ href: "https://github.com/gabrielapiovezan/", name: icon({ name: 'github' }), class: "link-footer", title: "Gabriela Piovezan", target: "_blank" })}
+            ${link({ href: "https://www.linkedin.com/in/gabrielapiovezan/", name: icon({ name: 'linkedin' }), class: "link-footer", title: "Gabriela Piovezan", target: "_blank" })}</div>
+          </span>
+        <div>
+          ${link({ href: "https://marianambarros.github.io/portifolio/src/", name: "Mariana Barros", class: "link-footer", title: "Mariana Barros", target: "_blank" })}
+         <span>
+            ${link({ href: "https://github.com/MarianaMBarros", name: icon({ name: 'github' }), class: "link-footer", title: "Mariana Barros", target: "_blank" })}
+            ${link({ href: "https://www.linkedin.com/in/marianambarros/", name: icon({ name: 'linkedin' }), class: "link-footer", title: "Mariana Barros", target: "_blank" })}
+          </span>
+        </div>  
+        </h5>    
+    </footer>
+    `;
 
-    // container.appendChild(icon('churrasqueira'))
-    // container.appendChild(icon('cafeteira'))
-    // container.appendChild(icon('comida'))
-    // container.appendChild(icon('luva'))
-    // container.appendChild(icon('talher'))
-    // container.appendChild(icon('tomate'))
-    // container.appendChild(icon('caneca'))
 
-
-    const greetingBtn = container.querySelector('#greeting-btn');
-    //const greetingMessage = container.querySelector('#greeting-message');
-
-
-
-    greetingBtn.addEventListener('click', (event) => {
+    container.querySelector('#post-btn').addEventListener('click', (event) => {
         event.preventDefault();
         const postText = container.querySelector('#post-text').value;
-        const post = {
-            name: firebase.auth().currentUser.displayName,
-            text: postText,
-            user_id: firebase.auth().currentUser.uid,
-            likes: 0,
-            liked: [],
-            comments: []
+
+        function saveFirebase(urlFile) {
+            const postText = container.querySelector('#post-text').value;
+            const post = {
+                url_file: `https://firebasestorage.googleapis.com/v0/b/social-networt.appspot.com/o/${urlFile}?alt=media`,
+                name: firebase.auth().currentUser.displayName,
+                text: postText,
+                user_id: firebase.auth().currentUser.uid,
+                likes: 0,
+                liked: [],
+                comments: [],
+                time: firebase.firestore.FieldValue.serverTimestamp()
+            }
         }
-        const postsCollection = firebase.firestore().collection("posts")
-        postsCollection.add(post).then(res => {
-            container.querySelector("#post-text").value = ""
-            loadPost()
-        })
+        container.querySelector("#post-text").value = "";
+        container.querySelector("#posts").innerHTML = "";
+        createPost(post);
+        // loadPost(addPosts, like, likeClass, deletePost, editPost);
     });
 
+    container.querySelector('#logout-btn').addEventListener('click', (event) => {
+        event.preventDefault();
+        logout();
+    })
 
 
+    function likeClass(id, valid) {
 
-    function likeClass(post) {
-        post.data().liked.forEach(a => {
-            if (a === firebase.auth().currentUser.uid) {
-                container.querySelector(`#like1${post.id}`).classList.add("disappear")
-                container.querySelector(`#like2${post.id}`).classList.remove("disappear")
-            } else {
-                container.querySelector(`#like2${post.id}`).classList.add("disappear")
-                container.querySelector(`#like1${post.id}`).classList.remove("disappear")
-            }
-        })
+        //  post.data().liked.forEach(a => {
+        if (valid === 1) {
+            container.querySelector(`#icon-dynamic-1-${id}`).classList.add("disappear");
+            container.querySelector(`#icon-dynamic-2-${id}`).classList.remove("disappear")
+        } else {
+            container.querySelector(`#icon-dynamic-2-${id}`).classList.add("disappear");
+            container.querySelector(`#icon-dynamic-1-${id}`).classList.remove("disappear");
+        }
 
-    }
-
+    };
+    //          ${iconColor({ name: 'cadeado', id: post.id })}
     function addPosts(post) {
         const postsTemplete = `
-        <li id="li${post.id}" class="post box">
-            <div class="user-post">Publicado por: ${post.data().name} ${icon({name:'talher', id:post.id+"close"})}</div>
-            ${post.data().text} 
-            <div id="likeid${post.id}" class="icon-post">${post.data().likes}${icon({name:'cereja', id:post.id})}</div>
+        <li id="li${post.id}" class="post box">        
+          <div class="user-post">
+          Publicado por: ${post.data().name} 
+            <div class="btn-post">
+              ${icon({ id: `edit${post.id}`, class: "edit-btn disappear", name: "edit" })}
+              ${icon({ id: `save${post.id}`, class: "edit-btn disappear", name: "checked" })}
+              ${icon({ name: 'talher', id: post.id })}</div>
+            </div>  
+          <div class="text" id="text${post.id}">${post.data().text}</div>          
+          <div id="like-number-${post.id}" class="icon-post">${post.data().likes} 
+          <span id="like${post.id}">${iconColor({ name: 'cereja', id: post.id })}</span>          
+          ${icon({ name: 'comentario', id: post.id })}</div> 
         </li>
-        `
-        container.querySelector("#posts").innerHTML += postsTemplete
+        `;
+    container.querySelector("#posts").innerHTML += postsTemplete;
+  };
+
+  function deletePost(post) {
+    container.querySelector(`#icon${post.id}`).addEventListener("click", (event) => {
+      event.preventDefault();
+      let postUser = post.data().user_id;
+
+      if (postUser === firebase.auth().currentUser.uid) {
+        postDelete(post.id);
+        container.querySelector("#posts").innerHTML = "";
+      //  loadPost(addPosts, like, likeClass, deletePost, editPost);
+      } else {
+        alert("Você não é o autor do post!");
+      };
+    });
+  };
+
+  function like(post) {
+    container.querySelector(`#like${post.id}`).addEventListener("click", (event) => {
+      event.preventDefault();
+      let likes = post.data().likes;
+      let likeUser = post.data().liked;
+      let valid = 1;
+
+      for (let i in likeUser) {
+        if (likeUser[i] === firebase.auth().currentUser.uid) {
+          likeUser.splice(i, 1);
+          valid = -1;
+        };
+      };
+
+      if (valid === 1) {
+        likeUser.push(firebase.auth().currentUser.uid);
+      };
+
+      likes += valid;
+      container.querySelector("#posts").innerHTML = "";
+      updateCollection(likeUser, likes, post.id); 
+      likeClass(post.id, valid)      
+
+   //   container.querySelector(`#like-number-${post.id}`).innerHTML = "";
+  // loadPost(addPosts, like, likeClass, deletePost, editPost);
+    });
+  };
+
+  function profile(data) {
+    container.querySelector("#nameUser").innerHTML = `Olá, ${data}!`;
+  };
+
+
+ container.querySelector("#loker").addEventListener('click',()=>{
+
+  likeClass("block", 1)
+ })
+
+
+  let editing = false
+
+  
+  function editPost(post) {
+    const edit = container.querySelector(`#edit${post.id}`)
+    const save = container.querySelector(`#save${post.id}`)
+
+    let postEdit = post.data().user_id;
+    if (postEdit === firebase.auth().currentUser.uid) {
+      edit.classList.remove("disappear")
+
+      edit.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (!editing) {
+          editing = true;
+          // const textPost = container.querySelector(`#text${post.id}`);
+
+          container.querySelector(`#text${post.id}`).classList.add("disappear");
+
+          const newPost = document.createElement("div");
+          newPost.id = "edit-post"
+          const textEdit = textarea({ id: "edit-post-text", type: "text", size: "500", placeholder: "Compartilhe sua publicação aqui!", value: post.data().text });
+          newPost.innerHTML = textEdit;
+
+          container.querySelector(`#text${post.id}`).after(newPost);
+
+          edit.classList.add("disappear")
+          save.classList.remove("disappear")
+
+          save.addEventListener("click", async () => {
+            const valor = newPost.firstElementChild.value;
+            container.querySelector(`#save${post.id}`).classList.add("disappear")
+            container.querySelector(`#edit${post.id}`).classList.remove("disappear")
+
+            container.querySelector(`#text${post.id}`).innerHTML = newPost.firstElementChild.value;
+            container.querySelector(`#text${post.id}`).classList.remove("disappear")
+            container.querySelector("#edit-post").remove()
+
+            await updatePost(post.id, valor);
+            //loadPost(addPosts, like, likeClass, deletePost, editPost)
+            editing = false;
+          })
+        } else alert("Você já está editando!");
+      });
     }
+  };
 
+  user();
+  dataUser(profile);
+  loadPost(addPosts, like, likeClass, deletePost, editPost);
 
-    function like(post) {
-        container.querySelector(`#likeid${post.id}`).addEventListener("click", (event) => {
-            event.preventDefault();
-            let likes = post.data().likes
-            let likeUser = post.data().liked
-
-            let valid = 1
-
-            for (let i in likeUser) {
-                if (likeUser[i] === firebase.auth().currentUser.uid) {
-                    likeUser.splice(i, 1)
-                    valid = -1
-                }
-            }
-
-            if (valid === 1) {
-                likeUser.push(firebase.auth().currentUser.uid)
-            }
-            likes += valid
-            container.querySelector("#posts").innerHTML = ""
-            updateCollection(likeUser, likes, post.id)
-
-            loadPost(addPosts, like, likeClass);
-
-        })
-
-    }
-
-
-
-    function profile(data) {
-        container.querySelector("#nameUser").innerHTML = data
-    }
-    //  user();
-
-    dataUser(profile);
-    loadPost(addPosts, like, likeClass)
-    return container;
+  return container;
 };
