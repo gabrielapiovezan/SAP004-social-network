72b9c9a7d5b0aabe3aee1083e1c9b73340630ada
Mariana Barros <mamirandabarros11@gmail.com>
2020-06-13 01:02:31 -0300

    img

--- public/pages/elementos/objetos/icon.js
+++ public/pages/elementos/objetos/icon.js
@@ -1,5 +1,5 @@
 export default (info) => {
     const icon = `
-<input type="image"  src="./pages/elementos/icones/${info.name}-1.png"class="icon" id="icon${info.id}"> `;
+<input type="image"  src="./pages/elementos/icones/${info.name}-1.png"class="icon ${info.class}" id="icon${info.id}"> `;
     return icon
 };
